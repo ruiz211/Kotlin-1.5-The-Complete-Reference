6dc4a432d4831f11dcf4a980891df0e33799f980
ruiz211 <ruiz.im211@gmail.com>
2021-03-04 18:05:01 -0500

    Booleans

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/I/basics/BasicTypes.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/I/basics/BasicTypes.kt
@@ -1,11 +1,16 @@
-package com.ruiz211.kotlin.i.basics
+package com.ruiz211.kotlinlanguage.i.basics
 
 //Numbers
 
 val one: Int = 1
 val threeBillion: Long = 3000000000L
-val oneByte: Byte = 1
-var oneDouble: Double = 1.0
-var oneFloat: Float = 1.0F
+val oneByte: Byte = Byte.MIN_VALUE
+var oneDouble: Double = one.toDouble()
+var oneFloat: Float = Float.MAX_VALUE
+
+
+//Booleans
 val oneBoolean: Boolean = false
+
+
 
