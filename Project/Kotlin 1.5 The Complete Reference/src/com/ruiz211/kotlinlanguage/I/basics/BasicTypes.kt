24aa8de406f5f92cb0b699581e2b57565d5ab24b
ruiz211 <ruiz.im211@gmail.com>
2021-03-04 18:01:21 -0500

    Numbers

---
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/I/basics/BasicTypes.kt
@@ -0,0 +1,11 @@
+package com.ruiz211.kotlin.i.basics
+
+//Numbers
+
+val one: Int = 1
+val threeBillion: Long = 3000000000L
+val oneByte: Byte = 1
+var oneDouble: Double = 1.0
+var oneFloat: Float = 1.0F
+val oneBoolean: Boolean = false
+
