3b0c2892bb6f7c57276f728aa5dd284a630d9895
ruiz211 <ruiz.im211@gmail.com>
2021-03-04 18:45:52 -0500

    Type casting

---
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/I/basics/TypeCasting.kt
@@ -0,0 +1,16 @@
+package com.ruiz211.kotlinlanguage.I.basics
+
+
+var values: Array<Any> = arrayOf("String", 11, true, 101F)
+
+fun main(args: Array<String>) {
+    for (v in values) {
+        if (v is String) println("String value")
+        if (v is Int) println("Int value")
+        if (v is Boolean) println("Boolean value")
+        if (v is Float) println("Float value")
+    }
+
+    values.forEach { any: Any -> println(any) }
+}
+
