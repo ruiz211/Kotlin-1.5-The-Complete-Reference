6abae5362d297a6de4a1f9be6a67d814e3f21466
ruiz211 <ruiz.im211@gmail.com>
2021-03-05 20:17:48 -0500

    Constructors

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Classes.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Classes.kt
@@ -1,2 +1,16 @@
 package com.ruiz211.kotlinlanguage.iii.classes
 
+fun main(args: Array<String>) {
+    val person = Person("Charles", "Xavier")
+    person.name = "John".also(::print)
+
+}
+
+class Person constructor(var name: String, val surname: String) {
+    private var people: MutableList<Person> = mutableListOf()
+
+    constructor(name: String, surname: String, parent: Person) : this(name, surname) {
+        parent.people.add(this)
+    }
+
+}
