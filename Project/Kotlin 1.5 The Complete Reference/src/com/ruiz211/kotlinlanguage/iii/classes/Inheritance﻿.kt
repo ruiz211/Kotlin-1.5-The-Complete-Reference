404f4f5f9af86383483a9f76115f32f951dfaed9
ruiz211 <ruiz.im211@gmail.com>
2021-03-05 20:39:58 -0500

    Inheritance

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Inheritance﻿.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Inheritance﻿.kt
@@ -1,2 +1,24 @@
 package com.ruiz211.kotlinlanguage.iii.classes
 
+fun main(args: Array<String>) {
+    val son = Son("George")
+    son.printInfo()
+}
+
+open class Father(surname: String) {
+    open val counter: Int = 11
+
+    open fun printInfo() {
+        println("Father info")
+    }
+}
+
+class Son(name: String) : Father(name) {
+    override var counter: Int = 37
+
+    override fun printInfo() {
+//        super.printInfo()
+        println("Son info")
+    }
+}
+
