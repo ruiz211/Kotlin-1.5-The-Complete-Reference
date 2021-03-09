5d3934f1554eaadff65e8915fb4154ae75d7acaf
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 18:22:52 -0500

    Classes

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Classes.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Classes.kt
@@ -2,15 +2,24 @@ package com.ruiz211.kotlinlanguage.iii.classes
 
 fun main(args: Array<String>) {
     val person = Person("Charles", "Xavier")
-    person.name = "John".also(::print)
+    val second = Person(person.name, person.surname, person)
+
+    person.name = "John".also(::println)
+    person.surname = "Chaplin".also(::println)
+
+    println(person.toString())
+    println(second.toString())
 
 }
 
-class Person constructor(var name: String, val surname: String) {
+class Person constructor(var name: String, var surname: String) {
     private var people: MutableList<Person> = mutableListOf()
 
     constructor(name: String, surname: String, parent: Person) : this(name, surname) {
         parent.people.add(this)
     }
 
+    override fun toString(): String {
+        return "Person(name='$name', surname='$surname')"
+    }
 }
