066229591b2e65fbc4b9caf1ae0c22d65ac00cb7
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 18:42:07 -0500

    Abstract classes

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/AbstractClass.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/AbstractClass.kt
@@ -1,8 +1,21 @@
 package com.ruiz211.kotlinlanguage.iii.classes
 
+fun main(args: Array<String>) {
+    var figure = Figure()
+    figure.draw()
+    figure = Rectangle()
+    figure.draw()
+}
+
 open class Figure {
-    open fun draw() {}
+    open fun draw() {
+        println("Drawing figure")
+    }
 }
 
 class Rectangle : Figure() {
+    override fun draw() {
+        println("Drawing rectangle")
+    }
 }
+
