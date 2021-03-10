57a91621a38490211e533a5bd069e0c2f94ac1ad
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 19:06:24 -0500

    Inheritance

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Inheritance﻿.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Inheritance﻿.kt
@@ -1,24 +1,34 @@
 package com.ruiz211.kotlinlanguage.iii.classes
 
 fun main(args: Array<String>) {
-    val son = Son("George")
+    val son = Son(name = "George")
     son.printInfo()
+
+    val father = Father(surname = "Washington")
+    father.printInfo()
 }
 
-open class Father(surname: String) {
+open class Father constructor(surname: String) {
     open val counter: Int = 11
+
+    init {
+        println("Father class")
+    }
 
     open fun printInfo() {
         println("Father info")
     }
 }
 
-class Son(name: String) : Father(name) {
+class Son constructor(name: String) : Father(name) {
     override var counter: Int = 37
 
+    init {
+        println("Son class")
+    }
+
     override fun printInfo() {
-//        super.printInfo()
+        super.printInfo()
         println("Son info")
     }
 }
-
