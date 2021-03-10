dbc4566d379a9d4effdfbf9cfe91652d7e0aa2bc
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 20:19:05 -0500

    Functional interfaces

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/FunctionalInterfaces﻿.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/FunctionalInterfaces﻿.kt
@@ -1,2 +1,10 @@
 package com.ruiz211.kotlinlanguage.iii.classes
 
+fun main(args: Array<String>) {
+    val facto: Factorial = Factorial { it * 12 }
+    println(facto.facto(12))
+}
+
+fun interface Factorial {
+    fun facto(number: Int): Int
+}
