dbf875d3dcc8939edcc7b414b8329a513c41d407
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 20:38:21 -0500

    Visibility modifiers

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/VisibilityModifiers.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/VisibilityModifiers.kt
@@ -1,2 +1,13 @@
 package com.ruiz211.kotlinlanguage.iii.classes
 
+class Visibility {
+    //    private means visible inside this class only (including all its members).
+    //
+    //    protected is the same as private but is also visible in subclasses.
+    //
+    //    internal means that any client inside this module who sees the declaring class sees its internal members.
+    //
+    //    public means that any client who sees the declaring class sees its public members.
+
+
+}
