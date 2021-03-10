fadd2b1e2a5427173d83e2a872f591f04fee6912
ruiz211 <ruiz.im211@gmail.com>
2021-03-09 19:26:31 -0500

    Properties

--- Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Properties﻿.kt
+++ Project/Kotlin 1.5 The Complete Reference/src/com/ruiz211/kotlinlanguage/iii/classes/Properties﻿.kt
@@ -1,2 +1,21 @@
 package com.ruiz211.kotlinlanguage.iii.classes
 
+fun main(args: Array<String>) {
+    val info = PersonInfo()
+    info.description = "lateinit property"
+    println(info.description)
+}
+
+class PersonInfo {
+    enum class Cities {
+        LONDON,
+        PARIS,
+        BERLIN,
+        MOSCOW
+    }
+
+    var fullname: String = "Sherlock Holmes"
+    var direction: String = "221-B Baker"
+    val city: Cities = Cities.LONDON
+    lateinit var description: String
+}
